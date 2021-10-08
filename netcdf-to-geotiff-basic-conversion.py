36d2a6c02850f58caf7dca939d8527391e4a7224
Mengdi Ke <kemengdi@gmail.com>
2021-10-08 18:03:33 -0500

    code

--- netcdf-to-geotiff-basic-conversion.py
+++ netcdf-to-geotiff-basic-conversion.py
@@ -1,32 +1,32 @@
 import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import xarray as xr
-import rioxarray as rio
-import seaborn as sns
-import geopandas as gpd
-import earthpy as et
+#import matplotlib.pyplot as plt
+#import rioxarray as rio
+#import seaborn as sns
+#import geopandas as gpd
+#import earthpy as et
 
-#load file
+# 1. Load netCDF file
 nc_file = xr.open_dataset('/Users/keke/Desktop/GIS/prob_lower_tercile_MJJA2021_20210515.nc')
 
-#view xarray object
+# 2. View netCDF file
 #print(nc_file)
 
-#View latitude and longitude values
+# 3. View latitude and longitude values
 lat_lst = nc_file["__xarray_dataarray_variable__"]["latitude"].values[:]
 lon_lst = nc_file["__xarray_dataarray_variable__"]["longitude"].values[:]
 #print('latitude',lat_lst)
 #print('longitude',lon_lst)
 
-
+# 4. View xarray values in netCDF file
 value = np.flip(nc_file['__xarray_dataarray_variable__'].T)
-#print(value)
 
+# 5. Set Raster data property 
 value = value.rio.set_spatial_dims('longitude','latitude')
 value.rio.crs
 value.rio.set_crs("epsg:4326")
 
-#convert from netcdf to geotiff
+# 6. Convert from netCDF to GeoTiff, and output GeoTiff file 
 value.rio.to_raster(r"to_raster.tiff")
